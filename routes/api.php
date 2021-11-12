c1aaf856ac4ef7b31e40ae471ec6861fff48b179
Anton Stepurko <stepurko.anton@gmail.com>
2021-11-12 06:07:46 +0300

    create routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'employees' => EmployeeController::class,
+    'departments' => DepartmentController::class
+]);
